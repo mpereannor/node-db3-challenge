c677654f4679b3459668e9f74d3dc6d5fe9f8085
chudnofskyATD <mpereannor@gmail.com>
2019-11-06 19:18:13 +0000

    find helper created and tested successfully

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,11 +11,19 @@ module.exports = {
 
 function find(){
 
+    return db('schemes')
 };
-function findById(id){};
+function findById(id){
+};
 function findSteps(id){};
 function add(scheme){};
 function update(changes, id){};
 function remove(id){};
 
 
+// function getPosts(userId) {
+//   return db('posts as p')
+//     .join('users as u', 'u.id', 'p.user_id')
+//     .select('p.id', 'u.username', 'p.contents')
+//     .where({ 'u.id': userId });
+// }
