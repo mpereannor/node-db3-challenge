7273ac992b6ec16b3100424e5d8da8262c8809a4
chudnofskyATD <mpereannor@gmail.com>
2019-11-06 16:08:39 +0000

    data-config.js created, scafolding for scheme-model.js created

---
+++ schemes/scheme-model.js
@@ -0,0 +1,21 @@
+const db = require('../data/data-config');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+};
+
+function find(){
+
+};
+function findById(id){};
+function findSteps(id){};
+function add(scheme){};
+function update(changes, id){};
+function remove(id){};
+
+
